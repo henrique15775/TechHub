10880bc9fe8b4dc5bdf3473b9bf8b97728aadfd6
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2022-09-28 17:07:21 -0300

    firstCommit

---
+++ src/pages/Celulares/index.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect} from 'react'
+import Footer from '../../components/Footer'
+import Card from '../../components/Card/Card'
+import Menu from '../../components/Menu/Menu'
+
+import "../../App.css"
+import api from '../../services/Api'
+import { useProduction } from '../../contexts/ProductContext'
+
+function Celulares(){
+  const {produtos, setProd} = useProduction()
+
+  useEffect(() =>{
+    const readProduction= async (name)=>{
+      const data = await api.readAll(name)
+      setProd(data)
+    }
+    readProduction('celulares')
+  },[setProd])
+
+
+  return (
+    <>
+      <Menu/>
+      <form id="research">
+        <i className="fas fa-search"></i>
+        <input type="text" name="pesquisa" id="pesquisa" />
+      </form>
+      <section className='ConteinerCard'>
+
+        {produtos.map((produto) => {
+          return <Card produto={produto} key={produto.id}/>
+        })}
+      </section>
+      <Footer/>
+    </>
+  );
+}
+
+export default Celulares;
---
+++ src/pages/Noteeboks/index.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect} from 'react'
+import Footer from '../../components/Footer'
+import Card from '../../components/Card/Card'
+import Menu from '../../components/Menu/Menu'
+
+import "../../App.css"
+import api from '../../services/Api'
+import { useProduction } from '../../contexts/ProductContext'
+export default function Notebooks(){
+  const {produtos, setProd} = useProduction()
+
+  useEffect(() =>{
+    const readProduction= async (name)=>{
+      const data = await api.readAll(name)
+      setProd(data)
+    }
+    readProduction('notebooks')
+  },[setProd])
+
+
+  return (
+    <>
+      <Menu/>
+      <form id="research">
+        <i className="fas fa-search"></i>
+        <input type="text" name="pesquisa" id="pesquisa" />
+      </form>
+      <section className='ConteinerCard'>
+
+        {produtos.map((produto) => {
+          return <Card produto={produto} key={produto.id}/>
+        })}
+      </section>
+      <Footer/>
+    </>
+  );
+}
